a1faddcf6cd6a8ad0ca5d1929e15bacc3b17fe69
devrolet <chetdhill@gmail.com>
2018-11-07 12:27:56 -0500

    Add DELETE /todo/:id route

---
+++ playground/mongoose-delete.js
@@ -0,0 +1,19 @@
+// Find more information at mongoosejs.com/docs/queries
+
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { Artist } = require('./../server/models/artist');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+Todo
+
+Todo.findByIdAndRemove('5be31d181b1b68e0c16be703').then((todo) => {
+    console.log(todo);
+});

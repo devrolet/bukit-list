e486035d727dbff9fc7aeaf97137b6924c995b3a
Chet Hill <chetdhill@gmail.com>
2019-06-20 18:29:45 -0400

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -4,23 +4,44 @@ const { SHA256 } = require('crypto-js');
 // JWT encoding using jsonwebtoken
 const jwt = require('jsonwebtoken');
 
+// Hashing & Salting Passwords using bcryptjs
+const bcrypt = require('bcryptjs');
+
+// Pass variable
+var password = '123abcd';
+
+// Gen salt
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$X5sU5ZiIzCyMYcretRAYbOqM8diBBYlLQ4xlefjW4FVmqQnfMpTCe';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// Hash
+
 // Set data (payload)
-var data = {
-        id: 18
-    }
-    // Go to jwt.io to check the data. HEADER, PAYLOAD, VERIFY
-    // JWT has two main functions (sign and verify)
-var token = jwt.sign(data, 'abc123');
-console.log(token);
+// var data = {
+//         id: 18
+//     }
+//     // Go to jwt.io to check the data. HEADER, PAYLOAD, VERIFY
+//     // JWT has two main functions (sign and verify)
+// var token = jwt.sign(data, 'abc123');
+// console.log(token);
 
-var decoded = jwt.verify(token, 'abc123');
-console.log('decoded', decoded);
+// var decoded = jwt.verify(token, 'abc123');
+// console.log('decoded', decoded);
 
-var message = 'I am the man around here';
-var hash = SHA256(message).toString();
+// var message = 'I am the man around here';
+// var hash = SHA256(message).toString();
 
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
 
 // var data = {
 //     id: 4
